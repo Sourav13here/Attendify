d94a65d01a5f797436ccee8281aeac21e301293b
Sourav13here <souravsharma13here@.com>
2025-03-30 22:36:52 +0530

    Colors added

--- app/src/main/java/com/example/attendify/ui/theme/Color.kt
+++ app/src/main/java/com/example/attendify/ui/theme/Color.kt
@@ -5,7 +5,43 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val White= Color(0xFFFFFFFF)
+val Whitty=Color(0xFFD9D9DC)
+val Bvec= Color(0xFFFC578B)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// 🔵 Blue Shades
+val OceanBlue = Color(0xFF0277BD)    // Deep ocean blue
+val SkyBlue = Color(0xFF81D4FA)      // Light sky blue
+
+// 🟢 Green Shades
+val EmeraldGreen = Color(0xFF2E7D32) // Deep emerald green
+val MintGreen = Color(0xFFA5D6A7)    // Soft mint green
+
+// 🔴 Red Shades
+val CrimsonRed = Color(0xFFB71C1C)   // Deep crimson red
+val CoralRed = Color(0xFFFF6F61)     // Soft coral red
+
+// 🟡 Yellow & Orange Shades
+val SunflowerYellow = Color(0xFFFFC107) // Bright sunflower yellow
+val SunsetOrange = Color(0xFFFF7043)    // Vibrant sunset orange
+
+// 🟣 Purple & Pink Shades
+val RoyalPurple = Color(0xFF6A1B9A)   // Deep royal purple
+val LavenderPurple = Color(0xFFD1C4E9) // Soft lavender purple
+val RosePink = Color(0xFFF06292)      // Delicate rose pink
+
+// ⚫ Neutral & Gray Shades
+val MidnightBlack = Color(0xFF212121)  // Almost black
+val GraphiteGray = Color(0xFF616161)   // Medium gray
+val SilverGray = Color(0xFFBDBDBD)     // Light silver gray
+
+// 🌊 Special Shades
+val Turquoise = Color(0xFF00ACC1)      // Bright turquoise blue
+val TealGreen = Color(0xFF00897B)      // Rich teal green
+val Gold = Color(0xFFFFD700)           // Luxurious gold
+val Bronze = Color(0xFFCD7F32)         // Elegant bronze
+val PearlWhite = Color(0xFFFFF8E1)     // Soft pearl white
