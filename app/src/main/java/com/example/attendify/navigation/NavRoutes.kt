585c45b2f5c039ce5147b5cddb43580c553da987
Mridul60 <mridulroy543@gmail.com>
2025-04-03 13:31:30 +0530

    Routes setup

--- app/src/main/java/com/example/attendify/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/attendify/navigation/NavRoutes.kt
@@ -1,7 +1,32 @@
 package com.example.attendify.navigation
 
-sealed class NavRoutes(val route: String) {
-    object LoginPage : NavRoutes("login")
+interface Destinations {
+    val route: String
 }
 
+object SignUpPage: Destinations {
+    override val route = "signup"
+}
 
+object LoginPage: Destinations {
+    override val route = "login"
+}
+
+object VerificationStatus: Destinations {
+    override val route = "VerificationStatus"
+}
+
+object VerificationPage: Destinations {
+    override val route = "VerifyPage"
+}
+
+object StudentDashboard: Destinations {
+    override val route = "StudentDashboard"
+}
+
+object TeacherDashboard: Destinations {
+    override val route = "TeacherDashboard"
+}
+object AddSubjects: Destinations{
+    override val route = "Add subject"
+}
