34faaf3178c1e309d56bf8dad984d40afb108d2d
Mridul60 <mridulroy543@gmail.com>
2025-04-05 20:47:53 +0530

    Comment added

--- app/src/main/java/com/example/attendify/ui/login/Login.kt
+++ app/src/main/java/com/example/attendify/ui/login/Login.kt
@@ -2,18 +2,21 @@ package com.example.attendify.ui.login
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,10 +32,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.attendify.R
 import com.example.attendify.common.composable.AppScaffold
 import com.example.attendify.common.composable.CustomButton
-import com.example.attendify.common.composable.CustomOutlinedTextField
-import com.example.attendify.common.composable.CustomTextButton
 import com.example.attendify.navigation.NavRoutes
-import com.example.attendify.ui.login.components.ForgetPasswordDialog
 import com.example.attendify.ui.login.components.UserLoginInfoCard
 import com.example.attendify.ui.theme.AttendifyTheme
 
@@ -77,7 +77,7 @@ fun Login(navController: NavController, viewModel: LoginViewModel) {
 
                 Spacer(modifier = Modifier.weight(1f))
 
-                // Sign up section
+                // BottomBar
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
