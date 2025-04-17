ca65431243e4e1cffcdb4362ce711c89dfa72b73
Mridul60 <mridulroy543@gmail.com>
2025-04-17 12:21:42 +0530

    Dynamic display of name in Verification_status.kt

--- app/src/main/java/com/example/attendify/navigation/NavRoutes.kt
+++ app/src/main/java/com/example/attendify/navigation/NavRoutes.kt
@@ -3,7 +3,12 @@ package com.example.attendify.navigation
 sealed class NavRoutes(val route: String) {
     object LoginPage : NavRoutes("login")
     object SignUpPage: NavRoutes("sign up")
-    object VerificationStatus: NavRoutes("verification status")
+    object VerificationStatus: NavRoutes("verification_status/{username}/{branch}/{semester}/{roll}"){
+        fun createRoute(username:String, branch:String,semester:String,roll:String ): String{
+            return   "verification_status/$username/$branch/$semester/$roll"
+
+        }
+    }
     object TeacherDashboard: NavRoutes("teacher dashboard")
     object StudentDashboard: NavRoutes("student dashboard")
     object SplashScreen: NavRoutes("splash screen")
