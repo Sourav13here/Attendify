e6d2c18086253c59e9cb7ee3b83108d96463e181
Mridul60 <mridulroy543@gmail.com>
2025-05-03 05:08:00 +0530

    UI Pt.2(Verification page)

--- app/src/main/java/com/example/attendify/ui/verification/Verification_Page.kt
+++ app/src/main/java/com/example/attendify/ui/verification/Verification_Page.kt
@@ -46,7 +46,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.attendify.common.composable.AppScaffold
@@ -68,9 +67,7 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
     val semesters = listOf("1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th")
 
     LaunchedEffect(selectedTab, selectedBranch, selectedSemester) {
-        if (selectedBranch != "Select Branch" &&
-            (selectedTab == 1 || selectedSemester != "Select Semester")
-        ) {
+        if (selectedBranch != "Select Branch" && (selectedTab == 1 || selectedSemester != "Select Semester")) {
 
             if (selectedTab == 0) {
                 viewModel.fetchUnverifiedUsers("student", selectedBranch, selectedSemester)
@@ -106,6 +103,7 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
                     .width(250.dp)
                     .align(Alignment.CenterHorizontally)
             ) {
+//                Selection Tab of Teacher and Student where 0 --> Student & 1 --> Teacher
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -137,7 +135,7 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
                     }
                 }
             }
-
+//            UNVERFIED DATA UPDATION BOX
             Box(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
@@ -159,43 +157,40 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
                     }
                     Spacer(modifier = Modifier.height(20.dp))
 
-                    DropdownSelector(
-                        label = selectedBranch,
+                    DropdownSelector(label = selectedBranch,
                         items = branches,
-                        onItemSelected = { selectedBranch = it }
-                    )
+                        onItemSelected = { selectedBranch = it })
 
                     Spacer(modifier = Modifier.height(8.dp))
 
                     if (selectedTab == 0) {
-                        DropdownSelector(
-                            label = selectedSemester,
+                        DropdownSelector(label = selectedSemester,
                             items = semesters,
-                            onItemSelected = { selectedSemester = it }
-                        )
+                            onItemSelected = { selectedSemester = it })
                     }
 
 
-
-
+                    //----------------------------Main content of the verification page-----------------------------------------------
                     if (selectedBranch == "Select Branch" || (selectedTab == 0 && selectedSemester == "Select Semester")) {
                         Box(
                             modifier = Modifier.fillMaxSize(),
                             contentAlignment = Alignment.Center,
                         ) {
-                            Text("Please select a branch${if (selectedTab == 0) " and semester" else ""} to view users.", textAlign = TextAlign.Center, color = Color.Gray)
+                            Text(
+                                "Please select a branch${if (selectedTab == 0) " and semester" else ""} to view users.",
+                                textAlign = TextAlign.Center,
+                                color = Color.Gray
+                            )
                         }
                     } else if ((selectedTab == 0 && students.isEmpty()) || (selectedTab == 1 && teachers.isEmpty())) {
                         Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
+                            modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                         ) {
                             Text("No unverified users found.")
                         }
                     } else if (isLoading) {
                         Box(
-                            modifier = Modifier.fillMaxSize(),
-                            contentAlignment = Alignment.Center
+                            modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
                         ) {
                             CircularProgressIndicator(color = Color.Black, strokeWidth = 2.dp)
                         }
@@ -206,33 +201,37 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
                             items(list) { user ->
                                 when (user) {
                                     is Student -> {
-                                        UserVerificationItem(
-                                            name = user.name,
+                                        UserVerificationItem(name = user.name,
                                             email = user.email,
                                             rollNumber = user.rollNumber,
                                             onApprove = {
                                                 viewModel.approveUser(
+                                                    user.uid, "student"
+                                                )
+                                            },
+                                            onReject = {
+                                                viewModel.rejectUser(
                                                     user.uid,
                                                     "student"
                                                 )
-                                            },
-                                            onReject = { viewModel.rejectUser(user.uid, "student") }
-                                        )
+                                            })
                                     }
 
                                     is Teacher -> {
-                                        UserVerificationItem(
-                                            name = user.name,
+                                        UserVerificationItem(name = user.name,
                                             email = user.email,
                                             rollNumber = null,
                                             onApprove = {
                                                 viewModel.approveUser(
+                                                    user.uid, "teacher"
+                                                )
+                                            },
+                                            onReject = {
+                                                viewModel.rejectUser(
                                                     user.uid,
                                                     "teacher"
                                                 )
-                                            },
-                                            onReject = { viewModel.rejectUser(user.uid, "teacher") }
-                                        )
+                                            })
                                     }
                                 }
                             }
@@ -248,11 +247,9 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
                     .padding(vertical = 16.dp),
                 contentAlignment = Alignment.Center
             ) {
-                CustomButton(
-                    text = "DONE",
+                CustomButton(text = "DONE",
                     modifier = Modifier.fillMaxWidth(0.5f),
-                    action = { navController.navigate(NavRoutes.TeacherDashboard.route) }
-                )
+                    action = { navController.navigate(NavRoutes.TeacherDashboard.route) })
             }
         }
     }
@@ -260,28 +257,22 @@ fun Verification_Page(navController: NavController, viewModel: VerificationViewM
 
 @Composable
 fun DropdownSelector(
-    label: String,
-    items: List<String>,
-    onItemSelected: (String) -> Unit
+    label: String, items: List<String>, onItemSelected: (String) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = Modifier
-            .clip(RoundedCornerShape(8.dp))
-            .background(Color(0xFFE0E0E0))
-            .clickable { expanded = true }
-            .padding(horizontal = 12.dp, vertical = 10.dp)
-    ) {
+    Box(modifier = Modifier
+        .clip(RoundedCornerShape(8.dp))
+        .background(Color(0xFFE0E0E0))
+        .clickable { expanded = true }
+        .padding(horizontal = 12.dp, vertical = 10.dp)) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(
-                text = label,
-                modifier = Modifier.weight(1f),
-                maxLines = 1
+                text = label, modifier = Modifier.weight(1f), maxLines = 1
             )
             Icon(
                 imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
@@ -295,13 +286,10 @@ fun DropdownSelector(
             modifier = Modifier.fillMaxWidth(0.8f)
         ) {
             items.forEach { item ->
-                DropdownMenuItem(
-                    text = { Text(item) },
-                    onClick = {
-                        onItemSelected(item)
-                        expanded = false
-                    }
-                )
+                DropdownMenuItem(text = { Text(item) }, onClick = {
+                    onItemSelected(item)
+                    expanded = false
+                })
             }
         }
     }
@@ -309,16 +297,13 @@ fun DropdownSelector(
 
 @Composable
 fun UserVerificationItem(
-    name: String,
-    email: String,
-    rollNumber: String? = null, // Optional for Teacher
-    onApprove: () -> Unit,
-    onReject: () -> Unit
+    name: String, email: String, rollNumber: String? = null, // Optional for Teacher
+    onApprove: () -> Unit, onReject: () -> Unit
 ) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 10.dp,)
+            .padding(vertical = 10.dp)
             .border(1.dp, Color.Black, RoundedCornerShape(4.dp))
             .background(Color(0xFFD9D9D9))
             .padding(6.dp)
@@ -331,16 +316,12 @@ fun UserVerificationItem(
             // User Info
             Column(modifier = Modifier.weight(1f)) {
                 Text(
-                    text = name,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Medium
+                    text = name, fontSize = 14.sp, fontWeight = FontWeight.Medium
                 )
 
                 rollNumber?.let {
                     Text(
-                        text = "Roll No: $it",
-                        fontSize = 14.sp,
-                        color = Color.DarkGray
+                        text = "Roll No: $it", fontSize = 14.sp, color = Color.DarkGray
                     )
                 }
 
@@ -351,9 +332,7 @@ fun UserVerificationItem(
                         withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
                             append(email)
                         }
-                    },
-                    fontSize = 14.sp,
-                    color = Color.DarkGray
+                    }, fontSize = 14.sp, color = Color.DarkGray
                 )
 
             }
@@ -363,13 +342,9 @@ fun UserVerificationItem(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                ApprovalButton(
-                    text = "Approve",
-                    color = Color(0xFF187C1A),
-                    onClick = {
-                        onApprove()
-                    }
-                )
+                ApprovalButton(text = "Approve", color = Color(0xFF187C1A), onClick = {
+                    onApprove()
+                })
                 ApprovalButton(text = "Reject", color = Color(0xFFCB0D0D), onClick = { onReject() })
             }
         }
@@ -390,35 +365,33 @@ private fun ApprovalButton(text: String, color: Color, onClick: () -> Unit) {
     }
 
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Box(
-            modifier = Modifier
-                .size(24.dp)
-                .clip(CircleShape)
-                .background(color.copy(0.5f))
-                .clickable { isClicked = true
-                    onClick() }
-                .border(2.dp,Color.White, CircleShape)
-        ){
+        Box(modifier = Modifier
+            .size(24.dp)
+            .clip(CircleShape)
+            .background(color.copy(0.5f))
+            .clickable {
+                isClicked = true
+                onClick()
+            }
+            .border(2.dp, Color.White, CircleShape)) {
             if (isClicked) {
                 Box(
-                    modifier = Modifier.align(Alignment.Center)
+                    modifier = Modifier
+                        .align(Alignment.Center)
                         .fillMaxSize(0.6f)
                         .background(color, CircleShape) // Solid black circle inside
                 )
             }
         }
         Text(
-            text = text,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(top = 2.dp)
+            text = text, fontSize = 12.sp, modifier = Modifier.padding(top = 2.dp)
         )
     }
 }
 
 @Composable
 fun SpecialMessage(
-    message: String,
-    modifier: Modifier = Modifier
+    message: String, modifier: Modifier = Modifier
 ) {
     Box(
         modifier = modifier
@@ -430,8 +403,7 @@ fun SpecialMessage(
         Box(
             modifier = Modifier
                 .background(
-                    Color(0xFFF5F5F5),
-                    shape = RoundedCornerShape(50) // Elliptical background
+                    Color(0xFFF5F5F5), shape = RoundedCornerShape(50) // Elliptical background
                 )
                 .border(2.dp, Color.Black, shape = RoundedCornerShape(50)) // Apply spiky border
                 .padding(20.dp) // Padding inside the box
