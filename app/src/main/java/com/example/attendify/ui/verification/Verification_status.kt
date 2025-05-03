9783e8a89499c2684eeae9a62d93387e5d67ca82
Mridul60 <mridulroy543@gmail.com>
2025-05-03 13:09:58 +0530

    optimised

--- app/src/main/java/com/example/attendify/ui/verification/Verification_status.kt
+++ app/src/main/java/com/example/attendify/ui/verification/Verification_status.kt
@@ -2,38 +2,49 @@ package com.example.attendify.ui.verification
 
 import android.util.Log
 import android.widget.Toast
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.automirrored.filled.Logout
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.*
-import androidx.compose.material3.DividerDefaults.color
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.attendify.R
 import com.example.attendify.common.composable.AppScaffold
-import com.example.attendify.common.composable.CustomIconButton
 import com.example.attendify.common.composable.LogoutButton
 import com.example.attendify.navigation.NavRoutes
-import com.example.attendify.ui.verification.components.LogoutConfirmationDialog
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Composable
 fun VerificationStatus(
@@ -51,7 +62,6 @@ fun VerificationStatus(
     val navigateToTeacherDashboard by viewmodel.navigateToTeacherDashboard.collectAsState()
     val userData by viewmodel.userData.collectAsState()
     val showEmailNotVerifiedBox by viewmodel.showEmailNotVerifiedBox.collectAsState()
-    var showLogOutDialog by remember { mutableStateOf(false) }
     val snackbarMessage by viewmodel.snackbarMessage.collectAsState()
     val snackbarHostState = remember { SnackbarHostState() }
 
